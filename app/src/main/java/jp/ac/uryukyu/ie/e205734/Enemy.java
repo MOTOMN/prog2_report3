bedf6edc2b2540bc355e06b001abbe97194cfa7e
e205734 <e205734@ie.u-ryukyu.ac.jp>
2020-11-23 17:55:03 +0900

    修正後

--- app/src/main/java/jp/ac/uryukyu/ie/e205734/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205734/Enemy.java
@@ -10,11 +10,34 @@ package jp.ac.uryukyu.ie.e205734;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
-
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+    public String getName(){
+        return this.name;
+    } 
+    public int getHitpoint(){
+        return this.hitPoint;
+    } 
+    public int getAttack(){
+        return this.attack;
+    } 
+    public boolean getDead(){
+        return this.dead;
+    } 
+    public void setName(String name){
+        this.name=name;
+    } 
+    public void setHitpoint(int hitPoint){
+        this.hitPoint=hitPoint;
+    } 
+    public void setAttack(int attack){
+        this.attack=attack;
+    } 
+    public void setDead(boolean dead){
+        this.dead=dead;
+    } 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
@@ -37,7 +60,7 @@ public class Enemy {
     public void attack(Hero hero){
         if( hitPoint > 0 ){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
         }
     }
